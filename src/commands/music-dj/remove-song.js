3d97ee698602c0ccd8c5f41f5639f31f399a7727
Christopher S <simmch@gmail.com>
2023-12-19 07:19:28 -0800

    Update remove-song.js

--- src/commands/music-dj/remove-song.js
+++ src/commands/music-dj/remove-song.js
@@ -7,6 +7,7 @@ const SONG_POSITION_OPTION_ID = 'song-position';
 
 module.exports = new ChatInputCommand({
   global: true,
+  permLevel: 'User',
   data: {
     description: 'Remove a song that is current in /queue',
     options: [

b6d63959a5cf73d812d2f22a3c4b3e421ee952c2
Christopher S <simmch@gmail.com>
2023-12-19 07:18:34 -0800

    Update play-next.js

--- src/commands/music-dj/play-next.js
+++ src/commands/music-dj/play-next.js
@@ -5,6 +5,7 @@ const { useMainPlayer, useQueue } = require('discord-player');
 
 module.exports = new ChatInputCommand({
   global: true,
+  permLevel: 'User',
   data: {
     description: 'Same as /play, but adds it to the front of the queue',
     options: [
